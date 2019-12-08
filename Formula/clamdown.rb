da747f9e7cd841ab5e982a92bc7e4e1f9a63d554
shekhar <shekhar@dripcapital.com>
2019-12-08 21:43:53 +0530

    update plist for launchd to run as a service

--- Formula/clamdown.rb
+++ Formula/clamdown.rb
@@ -4,10 +4,36 @@ class Clamdown < Formula
   url "https://github.com/shekhar-kamble/clamdown.git", tag: "0.1-alpha"
   head "https://github.com/shekhar-kamble/clamdown.git", branch: "master"
   version "0.1"
+
+  plist_options startup: true, manual: "clamdown"
+
   def install
     bin.install "clamdown"
   end
 
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+        <string>clamdown</string>
+      <key>ProgramArguments</key>
+        <array>
+            <string>bash</string>
+            <string>-c</string>
+            <string>#{opt_bin}/clamdown</string>
+        </array>
+      <key>ThrottleInterval</key>
+        <integer>2</integer>
+      <key>KeepAlive</key>
+      <true />
+      <key>StandardErrorPath</key>
+      <string>/dev/null</string>
+    </dict>
+    </plist>
+    EOS
+  end
   test do
     system bin/"clamdown"
   end
