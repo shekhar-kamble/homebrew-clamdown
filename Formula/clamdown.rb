1789170bb5f5b197fa3ff418286f5cb75314dbe2
shekhar <shekhar@dripcapital.com>
2019-12-08 20:38:32 +0530

    fix

--- Formula/clamdown.rb
+++ Formula/clamdown.rb
@@ -1,9 +1,9 @@
 class Clamdown < Formula
   desc "put your MacBook to sleep despite Apple's annoying clamshell mode"
   homepage "https://github.com/shekhar-kamble/clamdown"
-  url "https://github.com/shekhar-kamble/clamdown.git", tag: "1.0"
+  url "https://github.com/shekhar-kamble/clamdown.git", tag: "0.1-alpha"
   head "https://github.com/shekhar-kamble/clamdown.git", branch: "master"
-  version "1.0"
+  version "0.1"
   def install
     bin.install "clamdown"
   end
